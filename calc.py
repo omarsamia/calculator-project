d47f7fc35ea4de34e5a509fe73590f3381d12b84
Samia Omar <omarsamia7@gmail.com>
2020-07-09 11:00:43 -0400

    testing

---
+++ calc.py
@@ -0,0 +1,11 @@
+#Three steps to save to Github
+#First step: Add the files that you'd want to save
+git add[filename]
+OR
+git add . #adds all files
+
+#Second Step: Name this saved version
+git commit-m "name"
+
+#Third Step: Pusg the save to Github
+git push
